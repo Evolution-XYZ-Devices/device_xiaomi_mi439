b815ea09a8dd638add414ae0e61d11e5e9578f4e
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-01-02 17:30:53 +0800

    mi439: Determine device model

--- init/init_mi439.cpp
+++ init/init_mi439.cpp
@@ -6,6 +6,35 @@
 
 #include <libinit_sdm439.h>
 
+static const variant_info_t pine_info = {
+    .brand = "Xiaomi",
+    .device = "pine",
+    .marketname = "",
+    .model = "Redmi 7A",
+    .build_description = "pine-user 10 QKQ1.191014.001 V12.5.1.0.QCMCNXM release-keys",
+    .build_fingerprint = "Xiaomi/pine/pine:10/QKQ1.191014.001/V12.5.1.0.QCMCNXM:user/release-keys",
+};
+
+static const variant_info_t olive_info = {
+    .brand = "Xiaomi",
+    .device = "olive",
+    .marketname = "",
+    .model = "Redmi 8",
+    .build_description = "olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNCNXM release-keys",
+    .build_fingerprint = "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNCNXM:user/release-keys",
+};
+
+static void determine_device()
+{
+    std::string fdt_model;
+    android::base::ReadFileToString("/sys/firmware/devicetree/base/model", &fdt_model, true);
+    if (fdt_model.find("PINE QRD") != fdt_model.npos)
+        set_variant_props(pine_info);
+    else if (fdt_model.find("Olive QRD") != fdt_model.npos)
+        set_variant_props(olive_info);
+}
+
 void vendor_load_properties() {
     set_dalvik_heap_size();
+    determine_device();
 }
